ae6ec246de73244cc6b9133c110d9df0002e4869
Carl Littke <carl.littke@gmail.com>
2013-07-02 11:09:48 +0200

    Add package.js, server.js and client lib.

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: 'Braintree for Meteor'
+});
+
+Npm.depends({
+  braintree: "1.7.0",
+});
+
+Package.on_use(function(api) {
+  api.add_files('server.js', 'server');
+  api.add_files('braintree-client.js', 'client');
+});
